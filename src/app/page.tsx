0f310115b733230cb62411229fa8acb124617bbf
Mohammad Amin Rajabi <mohammadaminrajabii2004@gmail.com>
2025-01-04 00:51:12 +0330

    [UPDATE] Home Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,23 @@
+import GlobalSearchBox from "@/components/global-search-box/GlobalSearchBox";
+import MyDoctorLogo from "@/logo/my-doctor.logo";
+
+import styles from "./page.module.css";
+
 export default function Home() {
-  return <h1>سلام ،خوش آمدید</h1>;
+  return (
+    <div className={styles.home}>
+      <h1>
+        <MyDoctorLogo />
+        دکتر من
+      </h1>
+      <GlobalSearchBox />
+      <div className={styles.history}>
+        <div className={styles.title}>آخرین جستجوهای شما</div>
+        <ul>
+          <li>ارتوپد</li>
+          <li>قلب و عروق</li>
+        </ul>
+      </div>
+    </div>
+  );
 }
