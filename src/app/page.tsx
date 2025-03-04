fbb9794e82ed3bd70040e461230d024bd8b3d9fb
Mohammad Amin Rajabi <mohammadaminrajabii2004@gmail.com>
2025-03-04 14:40:24 +0330

    [ADD] dark mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,24 @@
-export default function Home() {
-  return <h1>به زودی</h1>
+type Post = {
+  id: number;
+  title: string;
+};
+
+export default async function Home() {
+  const res: Response = await fetch(
+    "https://jsonplaceholder.typicode.com/posts",
+    {
+      cache: "no-cache",
+    }
+  );
+  const posts: Post[] = await res.json();
+  return (
+    <div>
+      <h1>سلام</h1>
+      <ul>
+        {posts.map((post: Post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
