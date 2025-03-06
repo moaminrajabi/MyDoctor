23b28f76a3f7fd49f39f8cacfb94bc8ae27f89ea
Mohammad Amin Rajabi <mohammadaminrajabii2004@gmail.com>
2025-03-06 17:30:26 +0330

    [ADD] footer component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Vazirmatn } from "next/font/google";
 import Header from "./components/header/Header";
 
 import "./globals.css";
+import Footer from "./components/footer/Footer";
 
 const vazirmatn = Vazirmatn({
   subsets: ["latin", "arabic"],
@@ -25,6 +26,7 @@ export default function RootLayout({
       <body>
         <Header />
         <main> {children} </main>
+        <Footer />
       </body>
     </html>
   );
